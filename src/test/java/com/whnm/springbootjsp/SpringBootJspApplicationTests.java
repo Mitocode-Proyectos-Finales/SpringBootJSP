d56e63779c9660749c5be1794200fe31dd920d03
Wilson Hernande Neira Mija <wilsonnm22@hotmail.com>
2021-09-30 23:22:05 -0500

    commit inicial

---
+++ src/test/java/com/whnm/springbootjsp/SpringBootJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whnm.springbootjsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
